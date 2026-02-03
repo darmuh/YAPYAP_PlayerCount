72416f47f7bf00a5109d91e89e825ce0d4a54f04
darmuh <darmuh8@gmail.com>
2026-02-03 02:42:57 -0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,6 @@
+# PlayerCount (YAPYAP)
+
+### Modify the player lobby limit to up to 20 players (or down to 2!)
+
+- This is an incredibly simple mod that just modifies the lobby player limit from the default.
+- The limit is fully configurable and can both increase the limit up to 20 players or decrease the limit down to 2 players.
---
+++ src/YapYap-PlayerCount/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Mirror;
+using YAPYAP;
+
+namespace PlayerCount;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+    internal static ConfigEntry<int> MaxPlayers = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+
+        Log.LogMessage($"Plugin {Name} is loaded!");
+        MaxPlayers = Config.Bind("Settings", "Max Players", 10, new ConfigDescription("Set your desired maximum number of players here!", new AcceptableValueRange<int>(2, 20)));
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        Log.LogMessage($"Max players set to {MaxPlayers.Value}");
+    }
+
+    [HarmonyPatch(typeof(CoreFsmYap), nameof(CoreFsmYap.Setup))]
+    public class FsmYapHook
+    {
+        public static void Postfix(CoreFsmYap __instance)
+        {
+            __instance._maxPlayers = MaxPlayers.Value;
+        }
+    }
+
+    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartHost))]
+    public class MirrorNetManHook
+    {
+        public static void Prefix(NetworkManager __instance)
+        {
+            __instance.maxConnections = MaxPlayers.Value;
+        }
+    }
+}
